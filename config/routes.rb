d44862e22910ac66903f59d6c0bf3f49ecce8f77
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-11 10:29:56 -0400

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post 'articles', to: 'articles#create'
 
   get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch 'articles/:id', to: 'articles#edit'
+  patch 'articles/:id', to: 'articles#update'
 
   delete 'articles/:id', to: 'articles#destroy'
 end
